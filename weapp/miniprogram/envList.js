a331c47878a5ed40676537512c0db4772df0d312
onepiece1205 <773288239@qq.com>
2021-07-26 17:05:21 +0800

    first commit

---
+++ weapp/miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-8g1vg0am7cdd4f7f","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ weapp/uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-8g1vg0am7cdd4f7f --n quickstartFunctions --r --project "C:\projects\dewu_weapp\weapp"
