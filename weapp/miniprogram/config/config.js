7b56abe072b2cdb58a0e9a6156176cc0a71230b4
Tendo <3070568869@qq.com>
2021-07-27 23:23:43 +0800

    my_ltt

---
+++ weapp/miniprogram/config/config.js
@@ -0,0 +1,49 @@
+module.exports = {
+    "my_menu": {
+        "pay": [{
+            "url": '',
+            "title": "ltt的得物主页",
+            "icon": "/../images/my_icon/my1.png"
+        }],
+        "buy": [{
+            "url": '',
+            "title": "购买",
+            "icon": "/../images/my_icon/my1.png"
+        }],
+        "collect": [{
+                "url": '',
+                "title": "账户",
+                "icon": "/../images/my_icon/my1.png"
+            },
+            {
+                "url": '',
+                "title": "卡券",
+                "icon": "/../images/my_icon/my1.png"
+            },
+            {
+                "url": '',
+                "title": "地址管理",
+                "icon": "/../images/my_icon/my1.png"
+            },
+            {
+                "url": '',
+                "title": "消息通知",
+                "icon": "/../images/my_icon/my1.png"
+            },
+            {
+                "url": '',
+                "title": "客服中心",
+                "icon": "/../images/my_icon/my1.png"
+            },
+            {
+                "url": '',
+                "title": "隐私条款",
+                "icon": "/../images/my_icon/my1.png"
+            },
+        ],
+        "set": [{
+            "url": '',
+            "title": "退出登录",
+        }]
+    }
+}
