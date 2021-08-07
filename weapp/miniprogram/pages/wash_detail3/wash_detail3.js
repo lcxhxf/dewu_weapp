0867ae4d8618676c11811f33a6a7d9c2bda5ade1
罗超 <2422635277@qq.com>
2021-08-07 21:51:42 +0800

    lc

--- weapp/miniprogram/pages/wash_detail3/wash_detail3.js
+++ weapp/miniprogram/pages/wash_detail3/wash_detail3.js
@@ -1,11 +1,15 @@
-// miniprogram/pages/wash_detail3/wash_detail3.js
+ // pages/list/list.js
+const { heroList, nav } = require('../../data/hero')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    activeNavIndex:0,
+    heroNav: [],
+    heroList: [], //用于英雄列表 wx:for
+    allHero: []
   },
   return(e) {
     wx.switchTab({
@@ -13,11 +17,46 @@ Page({
     })
     
   },
+  changeHeros(e) {
+    console.log(e);
+    let {index, type} = e.currentTarget.dataset;
+    console.log(index, type);
+    this.setData({
+      activeNavIndex:index
+    })
+    // heroList
+    // 如何从一个数组中（每一项都是对象）, 有一个key camptype
+    // type  == camptype 找出所有来 
+    if (type == 'all') {
+      this.setData({
+        heroList:this.data.allHero
+      })
+    }else{
+      let heros = [];
+    this.data.allHero.forEach(hero => {
+      if (hero.camptype == type) {
+        heros.push(hero)
+      }
+    })
+    this.data.allHero.filter((hero) => hero.camptype == type)
+    this.setData({
+      heroList: heros
+    })
+    }
+    
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    setTimeout(() => {
+      this.setData({
+        heroNav: nav,
+        allHero: heroList,
+        heroList: heroList
+      })
+    }, 1000)
   },
 
   /**
@@ -52,7 +91,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
   },
 
   /**
