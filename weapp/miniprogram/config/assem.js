5394242cbf1f34be9e3602e7f4ba52eb124a0285
onepiece1205 <773288239@qq.com>
2021-08-08 15:15:01 +0800

    ghp/8/8.database

--- weapp/miniprogram/config/assem.js
+++ weapp/miniprogram/config/assem.js
@@ -16,5 +16,258 @@ module.exports = {
       "title": "筛选",
       "pic": "https://cdn.poizon.com/node-common/56a990155ead2e68885511813a820f95.png"
     },
+  ],
+  "people": [
+    {
+      "type": "0",
+      "kind": "通用"
+    },
+    {
+      "type": "1",
+      "kind": "女"
+    },
+    {
+      "type": "2",
+      "kind": "男"
+    },
+    {
+      "type": "3",
+      "kind": "大童"
+    },
+    {
+      "type": "4",
+      "kind": "婴童"
+    },
+    {
+      "type": "5",
+      "kind": "中童"
+    },
+  ],
+  "peoples": [
+    {
+      "type": "0",
+      "kind": "通用"
+    },
+    {
+      "type": "1",
+      "kind": "女"
+    },
+    {
+      "type": "2",
+      "kind": "男"
+    },
+    {
+      "type": "3",
+      "kind": "大童"
+    },
+    {
+      "type": "4",
+      "kind": "婴童"
+    },
+    {
+      "type": "5",
+      "kind": "中童"
+    },
+    {
+      "type": "6",
+      "kind": "儿童"
+    },
+  ],
+  "size": [
+    {
+      "type": "0",
+      "size": "34"
+    },
+    {
+      "type": "1",
+      "size": "34.5"
+    },
+    {
+      "type": "2",
+      "size": "35"
+    },
+    {
+      "type": "3",
+      "size": "35.5"
+    },
+    {
+      "type": "4",
+      "size": "36"
+    },
+    {
+      "type": "5",
+      "size": "36.5"
+    },
+  ],
+  "sizes": [
+    {
+      "type": "0",
+      "size": "34"
+    },
+    {
+      "type": "1",
+      "size": "34.5"
+    },
+    {
+      "type": "2",
+      "size": "35"
+    },
+    {
+      "type": "3",
+      "size": "35.5"
+    },
+    {
+      "type": "4",
+      "size": "36"
+    },
+    {
+      "type": "5",
+      "size": "36.5"
+    },
+    {
+      "type": "6",
+      "size": "37"
+    },
+    {
+      "type": "7",
+      "size": "37.5"
+    },
+    {
+      "type": "8",
+      "size": "38"
+    },
+    {
+      "type": "9",
+      "size": "38.5"
+    },
+    {
+      "type": "10",
+      "size": "39"
+    },
+    {
+      "type": "11",
+      "size": "39.5"
+    },
+    {
+      "type": "12",
+      "size": "40"
+    },
+    {
+      "type": "13",
+      "size": "40.5"
+    },
+
+    {
+      "type": "14",
+      "size": "41"
+    },
+    {
+      "type": "15",
+      "size": "41.5"
+    },
+    {
+      "type": "16",
+      "size": "42"
+    },
+  ],
+  "brand": [
+    {
+      "type": "0",
+      "kind": "Converse"
+    },
+    {
+      "type": "1",
+      "kind": "Nike"
+    },
+    {
+      "type": "2",
+      "kind": "Feiyue"
+    },
+    {
+      "type": "3",
+      "kind": "Vance"
+    },
+    {
+      "type": "4",
+      "kind": "Adidas"
+    },
+    {
+      "type": "5",
+      "kind": "Kappa"
+    },
+  ],
+  "brands": [
+    {
+      "type": "0",
+      "kind": "Converse"
+    },
+    {
+      "type": "1",
+      "kind": "Nike"
+    },
+    {
+      "type": "2",
+      "kind": "Feiyue"
+    },
+    {
+      "type": "3",
+      "kind": "Vance"
+    },
+    {
+      "type": "4",
+      "kind": "Adidas"
+    },
+    {
+      "type": "5",
+      "kind": "Kappa"
+    },
+    {
+      "type": "6",
+      "kind": "PONY"
+    },
+    {
+      "type": "7",
+      "kind": "MLB"
+    },
+    {
+      "type": "8",
+      "kind": "Levis"
+    },
+    {
+      "type": "9",
+      "kind": "PONY"
+    },
+    {
+      "type": "10",
+      "kind": "MLB"
+    },
+    {
+      "type": "11",
+      "kind": "Levis"
+    },
+    {
+      "type": "12",
+      "kind": "PONY"
+    },
+    {
+      "type": "13",
+      "kind": "MLB"
+    },
+    {
+      "type": "14",
+      "kind": "Levis"
+    },
+    {
+      "type": "15",
+      "kind": "PONY"
+    },
+    {
+      "type": "16",
+      "kind": "MLB"
+    },
+    {
+      "type": "17",
+      "kind": "Levis"
+    },
   ]
 }
