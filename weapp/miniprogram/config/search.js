ada5a065f276170c1d57603e3b8a8b0b55dd6073
onepiece1205 <773288239@qq.com>
2021-08-12 11:09:44 +0800

    搜索.8/12

---
+++ weapp/miniprogram/config/search.js
@@ -0,0 +1,30 @@
+var data = {
+  "hotList": [
+    {
+      "id": 0,
+      "text": "AJ11蓝白扣"
+    },
+    {
+      "id": 1,
+      "text": "冰激凌"
+    },
+    {
+      "id": 2,
+      "text": "T恤"
+    },
+    {
+      "id": 3,
+      "text": "冰激凌"
+    },
+    {
+      "id": 4,
+      "text": "LV"
+    },
+    {
+      "id": 5,
+      "text": "冰激凌"
+    },
+  ]
+}
+
+module.exports = data
