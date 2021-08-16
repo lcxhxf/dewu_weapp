bfcb3a42cc6858198cbbe10664dfd4f42113a69e
Tendo <3070568869@qq.com>
2021-08-16 20:34:43 +0800

    serviceCenter_ltt

--- weapp/miniprogram/pages/my_page/serviceCenter/serviceCenter.js
+++ weapp/miniprogram/pages/my_page/serviceCenter/serviceCenter.js
@@ -5,9 +5,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    activeNames: ['1'],
   },
-
+  onChange(event) {
+    this.setData({
+      activeNames: event.detail,
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
